eeec047d84af5df7e54f827c8ccf0b5f9a85c37f
gorantlasivakrishna <gorantla150@gmail.com>
2023-12-12 23:20:07 +0530

    Created mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 DATE=$(date +%F)
-LOGDIR=/tmp
+LOGSDIR=/tmp
 
 SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$0-$0DATE.log
+LOGFILE=$LOGSDIR/$0-$0DATE.log
 USERID=$(id -u)
 R="/e[31m"
 G="/e[32m"
 N="/e[0m"
 Y="/e[33m"
 
-if [$USERID -ne 0];
+if [ $USERID -ne 0 ];
 then
     echo -e "$R ERROR:: Please run this Script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
-if [$1 -ne 0]
-then
-    echo -e "$2 ... $R FAILURE $N"
-    exit 1
-else
-    echo -e "$2 ... $G SUCCESS $N"
-fi
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
