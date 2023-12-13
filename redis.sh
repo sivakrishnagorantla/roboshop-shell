e9e77b48871187b49d5e6738c6b6497b372fd18d
gorantlasivakrishna <gorantla150@gmail.com>
2023-12-13 23:13:33 +0530

    Created mongodb, Catalogue, redis and user

--- redis.sh
+++ redis.sh
@@ -29,12 +29,24 @@ VALIDATE(){
 
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
 
+VALIDATE $? "Installing redis repo"
+
 yum module enable redis:remi-6.2 -y &>>$LOGFILE
+
+VALIDATE $? "Enabling redis 6.2"
 
 yum install redis -y &>>$LOGFILE
 
+VALIDATE $? "Installing redis"
+
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$LOGFILE
+
+VALIDATE $? "Allowing remote connection"
 
 systemctl enable redis &>>$LOGFILE
 
+VALIDATE $? "Enabling redis"
+
 systemctl start redis &>>$LOGFILE
+
+VALIDATE $? "Startinh redis"
--- user.sh
+++ user.sh
@@ -29,32 +29,62 @@ VALIDATE(){
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
 
+VALIDATE $? "Downloading nodejs"
+
 yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing nodejs"
 
 useradd roboshop &>>$LOGFILE
 
+VALIDATE $? "Adding User"
+
 mkdir /app &>>$LOGFILE
+
+VALIDATE $? "Creating the directory"
 
 curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>>$LOGFILE
 
+VALIDATE $? "Downloading user articraft"
+
 cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app directory"
 
 unzip /tmp/user.zip &>>$LOGFILE
 
+VALIDATE $? "Unzipping userfile"
+
 cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving into app folder"
 
 npm install &>>$LOGFILE 
 
+VALIDATE $? "Installing dependies"
+
 cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>$LOGFILE
+
+VALIDATE $? "Copying user.service"
 
 systemctl daemon-reload &>>$LOGFILE
 
+VALIDATE $? "daemon-reloaded"
+
 systemctl enable user &>>$LOGFILE 
+
+VALIDATE $? "Enabling user"
 
 systemctl start user &>>$LOGFILE
 
+VALIDATE $? "Starting User"
+
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
+VALIDATE $? "Copying mongo repo"
+
 yum install mongodb-org-shell -y &>>$LOGFILE
+
+VALIDATE $? "Installing mongo client"
 
 mongo --host mongodb.sivakrishnawsdevops.online </app/schema/user.js &>>$LOGFILE
